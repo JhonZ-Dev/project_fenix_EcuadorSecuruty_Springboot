f438d24ce3bc4591a3473fe2ae3100ec1a339572
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-01-26 18:25:59 -0500

    Create Proyect

---
+++ mongo_plan_fenix/src/main/java/com/example/mongo_plan_fenix/MongoPlanFenixApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongo_plan_fenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoPlanFenixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoPlanFenixApplication.class, args);
+    }
+
+}
---
+++ mongo_plan_fenix/src/test/java/com/example/mongo_plan_fenix/MongoPlanFenixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongo_plan_fenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoPlanFenixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
