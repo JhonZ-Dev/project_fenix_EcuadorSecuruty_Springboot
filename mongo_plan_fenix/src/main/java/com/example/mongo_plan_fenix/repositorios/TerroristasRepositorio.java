4c1fb6ed8106fff6324f5ba74933717cd8970d08
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-01-26 20:27:34 -0500

    Update TerroristasRepositorio.java

--- mongo_plan_fenix/src/main/java/com/example/mongo_plan_fenix/repositorios/TerroristasRepositorio.java
+++ mongo_plan_fenix/src/main/java/com/example/mongo_plan_fenix/repositorios/TerroristasRepositorio.java
@@ -1,4 +1,15 @@
 package com.example.mongo_plan_fenix.repositorios;
 
-public interface TerroristasRepositorio {
+import com.example.mongo_plan_fenix.Colecciones.TerroristasColeccion;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Optional;
+
+public interface TerroristasRepositorio extends MongoRepository<TerroristasColeccion, Integer>
+
+{
+    @Query(value = "{}", sort = "{id_terroristas: -1}")
+    Optional<TerroristasColeccion> findTopByOrderByIdTerroristasDesc();
+
 }
