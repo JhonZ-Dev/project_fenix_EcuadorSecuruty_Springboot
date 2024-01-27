df6cb2e4e46091ff1a06770a963e9102da89c9f3
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-01-26 19:37:15 -0500

    Update TerroristasColeccion.java

--- mongo_plan_fenix/src/main/java/com/example/mongo_plan_fenix/Colecciones/TerroristasColeccion.java
+++ mongo_plan_fenix/src/main/java/com/example/mongo_plan_fenix/Colecciones/TerroristasColeccion.java
@@ -1,4 +1,23 @@
 package com.example.mongo_plan_fenix.Colecciones;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+
+@Document(collection = "Terroristas")
+@Data
 public class TerroristasColeccion {
+    @Id
+    @Indexed(unique=true)
+    private int id_terroristas;
+    private String nombre;
+    private String apellido;
+    private LocalDate fecha_nacimiento;
+    private String descripcion;
+    private String num_telefono_familiar;
+
 }
